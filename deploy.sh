b5e52bedfc1892dd484f27b57d943c277efd6a37
jerryqii <630751892@qq.com>
2019-11-13 16:20:25 +0800

    before subtree

--- deploy.sh
+++ deploy.sh
@@ -19,11 +19,19 @@ cd docs/.vuepress/dist
 # 如果你是要部署到自定义域名
 echo 'jerryqi.cn' > CNAME
 
-git init
-git add -A
-git commit -m "$deploycomment"
+# git init
+# git add -A
+# git commit -m "$deploycomment"
+git subtree split -P ./ -b {新分支名称}
 
 # 如果你想要部署到 https://<USERNAME>.github.io
 git push -f git@github.com:jerryqii/jerryqii.github.io.git master
 
 cd -
+
+git subtree pull --prefix=docs/.vuepress/dist git@github.com:jerryqii/jerryqii.github.io.git master --squash
+git subtree push --prefix=docs/.vuepress/dist git@github.com:jerryqii/jerryqii.github.io.git master
+# git remote add -f github git@github.com:jerryqii/jerryqii.github.io.git
+# git subtree add --prefix=docs/.vuepress/dist github master --squash
+# git subtree pull --prefix=docs/.vuepress/dist github master --squash
+# git subtree push --prefix=docs/.vuepress/dist github master
