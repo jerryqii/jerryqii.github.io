db30f22496ac7af75200a579b1d677f030832605
jerryqii <630751892@qq.com>
2019-11-13 15:27:15 +0800

    Deploy at 2019-11-13 15:27:10

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# 终止一个错误
+set -e
+
+dd=$(date '+%Y-%m-%d %H:%M:%S')
+deploycomment="Deploy at ${dd}"
+
+# 构建
+npm run build
+
+git add -A
+git commit -m "$deploycomment"
+git push origin master
+
+# 进入生成的构建文件夹
+cd docs/.vuepress/dist
+
+# 如果你是要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m "$deploycomment"
+
+# 如果你想要部署到 https://<USERNAME>.github.io
+git push -f git@github.com:jerryqii/jerryqii.github.io.git master
+
+cd -
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Jerry\'s Blog',
+  description: 'Jerry\'s Blog',
+  base: '/',
+  theme: 'homesite',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Blog', link: '/blog/' },
+      { text: 'Gitee', link: 'https://gitee.com/jerryqi' },
+    ]
+  }
+}
